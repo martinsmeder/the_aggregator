98201bf08edc104aa41930b5cb83924f25c381f4
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-08-08 22:45:41 +0200

    Added feeds

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const rssFeedUrl = 'https://news.mit.edu/topic/mitmachine-learning-rss.xml';
+// https://openai.com/blog/rss.xml
+// https://www.deepmind.com/blog/rss.xml
+// https://news.mit.edu/topic/mitmachine-learning-rss.xml
+// API: apnews, bbc
+
+const rssFeedUrl = 'https://openai.com/blog/rss.xml';
 const apiUrl = `https://rss-to-json-serverless-api.vercel.app/api?feedURL=${rssFeedUrl}`;
 
 // Make the GET request to the API
@@ -8,22 +13,15 @@ fetch(apiUrl)
     console.log(data);
 
     // Assuming data.items is an array of articles
-    const item = data.items[2]; // Select the first item for example
+    const item = data.items[0]; // Select the first item for example
 
     // Set article title, author, date, and content
     document.getElementById('article-title').textContent = item.title;
-    document.getElementById('article-author').textContent = item.author;
+    document.getElementById('article-description').textContent =
+      item.description;
     document.getElementById('article-date').textContent = new Date(
       item.published
     ).toLocaleDateString();
-    document.getElementById('article-content').innerHTML = item.content;
-
-    // Set article image
-    const articleImage = document.getElementById('article-image');
-    if (item.media && item.media.content && item.media.content.url) {
-      articleImage.src = item.media.content.url;
-      articleImage.alt = item.title; // Use article title as image alt text
-    }
   })
   .catch((error) => {
     console.error('An error occurred:', error);
