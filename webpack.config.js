e43f012dbf515d81a5fab69fdcfb99505b5c0d0b
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-08-10 20:02:45 +0200

    Test commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/script.js",
+  devtool: "inline-source-map", // Enable multi-file debugging
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
