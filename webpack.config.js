1da3b63b8398ce40efc36a056c1b9fd56442bcb1
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-08-11 21:47:45 +0200

    Added check to avoid duplicates in database

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,6 @@ module.exports = {
   entry: "./src/script.js",
   devtool: "inline-source-map", // Enable multi-file debugging
   plugins: [new Dotenv()],
-  resolve: {
-    alias: {
-      // Enable easier imports from config folder
-      config: path.resolve(__dirname, "config"),
-    },
-  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
