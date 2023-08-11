cb667714062c3ca66aa23d0440f57a9c7592bb92
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-08-11 19:00:53 +0200

    Added firebase to project

---
+++ src/firebase.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  setDoc,
+} from "firebase/firestore";
+// import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "the-aggregator-b51bd.firebaseapp.com",
+  projectId: "the-aggregator-b51bd",
+  storageBucket: "the-aggregator-b51bd.appspot.com",
+  messagingSenderId: "280028868322",
+  appId: "1:280028868322:web:f654a928da9628d6c378bb",
+  measurementId: "G-408NCFQLCF",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+const db = getFirestore(app);
+
+export { app, db, collection, doc, getDoc, getDocs, setDoc };
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,16 @@
 const path = require("path");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   entry: "./src/script.js",
   devtool: "inline-source-map", // Enable multi-file debugging
+  plugins: [new Dotenv()],
+  resolve: {
+    alias: {
+      // Enable easier imports from config folder
+      config: path.resolve(__dirname, "config"),
+    },
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
