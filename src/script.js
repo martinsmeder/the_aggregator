7eeab3d8094e80cccb05bc8fb33300f563e126dc
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-08-12 15:06:05 +0200

    Further refactoring

--- src/script.js
+++ src/script.js
@@ -5,6 +5,7 @@ import {
   addDoc,
   getDoc,
   getDocs,
+  writeBatch,
   query,
   where,
   deleteDoc,
@@ -15,6 +16,8 @@ import {
 // API: apnews, bbc
 
 const rssFeeder = (() => {
+  let existingTitlesSet = new Set();
+
   const feedUrls = [
     { openai: "https://openai.com/blog/rss.xml" },
     { deepmind: "https://www.deepmind.com/blog/rss.xml" },
@@ -57,12 +60,11 @@ const rssFeeder = (() => {
   function getExistingTitles() {
     return getDocs(collection(db, "ai"))
       .then((querySnapshot) => {
-        const titles = [];
         querySnapshot.forEach((doc) => {
-          titles.push(doc.data().title);
+          existingTitlesSet.add(doc.data().title);
         });
-        console.log(titles);
-        return titles;
+        console.log(existingTitlesSet);
+        return existingTitlesSet;
       })
       .catch((error) => {
         console.error("Error getting existing titles:", error);
@@ -70,8 +72,46 @@ const rssFeeder = (() => {
       });
   }
 
-  function addToFirestore() {
-    // ...
+  function addToFirestore(parsedData) {
+    return getExistingTitles()
+      .then(() => {
+        const promises = [];
+
+        parsedData.forEach((item) => {
+          const titleToCheck = item.title;
+
+          if (!existingTitlesSet.has(titleToCheck)) {
+            const promise = addDoc(collection(db, "ai"), {
+              title: item.title,
+              link: item.link,
+              description: item.description,
+              source: item.source,
+              date: item.date,
+            })
+              .then(() => {
+                console.log(`Document '${titleToCheck}' written successfully!`);
+                existingTitlesSet.add(titleToCheck);
+              })
+              .catch((error) => {
+                console.error("Error adding document to Firestore:", error);
+              });
+
+            promises.push(promise);
+          } else {
+            console.log(
+              `Document '${titleToCheck}' already exists, skipping...`
+            );
+          }
+        });
+
+        return Promise.all(promises);
+      })
+      .then(() => {
+        existingTitlesSet.clear();
+      })
+      .catch((error) => {
+        console.error("An error occurred:", error);
+      });
   }
 
   function deleteOldData() {
@@ -81,75 +121,22 @@ const rssFeeder = (() => {
   return {
     feedUrls,
     fetchData,
-    getExistingTitles,
+    addToFirestore,
   };
 })();
 
-rssFeeder.fetchData("https://openai.com/blog/rss.xml");
-rssFeeder.getExistingTitles();
+rssFeeder
+  .fetchData("https://openai.com/blog/rss.xml")
+  .then((parsedData) => {
+    return rssFeeder.addToFirestore(parsedData);
+  })
+  .catch((error) => {
+    console.error("An error occurred:", error);
+  });
 
 // https://openai.com/blog/rss.xml
 // https://www.deepmind.com/blog/rss.xml
 // https://news.mit.edu/topic/mitmachine-learning-rss.xml
-
-// ==========================================================================================
-
-// const rssFeedUrl = "https://news.mit.edu/topic/mitmachine-learning-rss.xml";
-// const apiUrl = `https://rss-to-json-serverless-api.vercel.app/api?feedURL=${rssFeedUrl}`;
-
-// // Make the GET request to the API
-// fetch(apiUrl)
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log(data);
-//     console.log(data.items.length);
-
-//     // Get the titles of existing documents in Firestore
-//     getDocs(collection(db, "ai"))
-//       .then((querySnapshot) => {
-//         const existingTitles = new Set();
-//         querySnapshot.forEach((doc) => {
-//           existingTitles.add(doc.data().title);
-//         });
-
-//         // Loop entire data.items array
-//         for (let i = 0; i < data.items.length; i++) {
-//           console.log(i);
-//           const item = data.items[i]; // Select the item
-//           const titleToCheck = item.title;
-
-//           if (!existingTitles.has(titleToCheck)) {
-//             // Document with the title doesn't exist, add it
-//             addDoc(collection(db, "ai"), {
-//               title: item.title,
-//               link: item.link,
-//               description: item.description || "No description available",
-//               source: data.title,
-//               date: new Date(item.published),
-//             })
-//               .then(() => {
-//                 console.log(`Document '${titleToCheck}' written successfully!`);
-//               })
-//               .catch((error) => {
-//                 console.error("Error adding document to Firestore:", error);
-//               });
-//           } else {
-//             console.log(
-//               `Document '${titleToCheck}' already exists, skipping...`
-//             );
-//           }
-//         }
-//       })
-//       .catch((error) => {
-//         console.error(
-//           "Error fetching existing documents from Firestore:",
-//           error
-//         );
-//       });
-//   })
-//   .catch((error) => {
-//     console.error("An error occurred:", error);
-//   });
 
 // ==========================================================================================
 
