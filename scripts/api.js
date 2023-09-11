206b24be582362cc6ba7d1e3cdbf34a507d36cb8
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-09-11 09:50:45 +0200

    Added comments

--- scripts/api.js
+++ scripts/api.js
@@ -3,51 +3,62 @@
 
 // Reuters:
 // Categories: Tech, business, science, world
-// Create one request for each page
-// Get data from successfull requests and error "no data on page" if empty
+// ---
+// ---
 // Combine into one array
 // Get the categories i want using filter
 // Get the data i want using map
 // Make sure I'm not getting any old data
 
 // const apiKey = process.env.NEWS_API_KEY;
-const apiUrl = "https://newsapi.org/v2/";
-const source = "reuters";
-const apiKey = "???";
+// const apiUrl = "https://newsapi.org/v2/";
+// const source = "reuters";
+// const apiKey = "???";
 
-function fetchPage(page) {
-  return fetch(
-    `${apiUrl}everything?sources=${source}&page=${page}&apiKey=${apiKey}`
-  ).then((response) => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json();
-  });
-}
+// function fetchPage(page) {
+//   return fetch(
+//     `${apiUrl}everything?sources=${source}&page=${page}&apiKey=${apiKey}`
+//   ).then((response) => {
+//     if (!response.ok) console.error(`Http error: ${response.status}`);
+//     return response.json();
+//   });
+// }
 
-function fetchAllPages() {
-  let page = 1;
-  const results = [];
+// function fetchAllPages() {
+//   let page = 1;
+//   const results = [];
 
-  function fetchNextPage() {
-    return fetchPage(page).then((data) => {
-      if (data.articles.length === 0) {
-        return results;
-      }
-      results.push(data);
-      page++;
-      return fetchNextPage();
-    });
-  }
+//   function fetchNextPage() {
+//     return fetchPage(page).then((data) => {
+//       // If no more items in array, stop calling and return results
+//       if (data.articles.length === 0) return results;
+//       results.push(data.articles);
+//       page++;
+//       // Recursively call itself until no more items in array
+//       return fetchNextPage();
+//     });
+//   }
+//   return fetchNextPage();
+// }
 
-  return fetchNextPage();
-}
+// fetchAllPages()
+//   .then((dataArray) => console.log(dataArray))
+//   .catch((error) => console.error(error));
 
-fetchAllPages()
-  .then((dataArray) => {
-    console.log("All pages fetched:", dataArray);
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+// ========================
+
+// function parse(dataArray) {
+//   const parsed = dataArray
+//     .flat()
+//     .filter((item) => item.id <= 10)
+//     .map((item) => item.email);
+
+//   return parsed;
+// }
+
+// // Call fetchAllPages, which returns a promise
+// // When the promise is resolved, log the resulting array to the console
+// fetchAllPages()
+//   .then((dataArray) => parse(dataArray))
+//   .then((parsed) => console.log(parsed))
+//   .catch((error) => console.error(error));
