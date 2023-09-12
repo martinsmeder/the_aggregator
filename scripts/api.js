78560599c4a4949c7916860de45ac36b7527c188
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-09-12 08:50:53 +0200

    Added filtering of results based on category

--- scripts/api.js
+++ scripts/api.js
@@ -5,9 +5,10 @@
 // Categories: Tech, business, science, world
 // ---
 // ---
-// Combine into one array
-// Get the categories i want using filter
-// Get the data i want using map
+// ---
+// ---
+// ---
+// I'm mostly getting business from reuters, no science, new approach needed
 // Make sure I'm not getting any old data
 // Make sure I'm not getting any duplicates
 
@@ -25,45 +26,39 @@ function fetchPage(page) {
   });
 }
 
-// .map((item) => {
-//     const date = new Date(item.published);
-//     return {
-//       date: date.toLocaleString(),
-//       title: item.title,
-//       link: item.link,
-//       description: item.description || "No description available",
-//       source: array.title,
-//       category: category,
-//       timestamp: new Date(item.published).getTime(),
-//     };
-//   })
-
 function parse(dataArray) {
-  const parsed = dataArray.flat().map((item) => {
-    const date = new Date(item.publishedAt);
-    return {
-      date: date.toLocaleString(),
-      title: item.title,
-      link: item.url,
-      description: item.description || "No description available",
-      source: item.source.name,
-      category: "?",
-      content: item.content,
-      timestamp: new Date(item.publishedAt).getTime(),
-    };
-  });
+  const parsed = dataArray
+    .flat()
+    .map((item) => {
+      const date = new Date(item.publishedAt);
+      if (item.url.includes("/science/")) item.category = "science";
+      else if (item.url.includes("/technology/")) item.category = "technology";
+      else if (item.url.includes("/business/")) item.category = "business";
+      else if (item.url.includes("/world/")) item.category = "world";
+      else item.category = "other";
+      return {
+        date: date.toLocaleString(),
+        title: item.title,
+        link: item.url,
+        description: item.description || "No description available",
+        source: item.source.name,
+        category: item.category,
+        content: item.content,
+        timestamp: new Date(item.publishedAt).getTime(),
+      };
+    })
+    .filter((item) => {
+      if (
+        item.category === "science" ||
+        item.category === "technology" ||
+        item.category === "business" ||
+        item.category === "world"
+      )
+        return item;
+    });
 
   return parsed;
 }
-
-// author: "Reuters"
-// content: "BERLIN, Aug 11 (Reuters) - UBS (UBSG.S) said in a memo to employees seen by Reuters on Friday that it would provide information on some more milestones it has reached in its merger with Credit Suisse… [+461 chars]"
-// description: "UBS <a href=\"https://www.reuters.com/markets/companies/UBSG.S\" target=\"_blank\">(UBSG.S)</a> said in a memo to employees seen by Reuters on Friday that it would provide information on some more milestones it has reached in its merger with Credit Suisse as part…"
-// publishedAt: "2023-08-11T05:57:38Z"
-// source: {id: 'reuters', name: 'Reuters'}
-// title: "UBS tells employees: more milestones in CS merger to be announced Aug 31"
-// url: "https://www.reuters.com/business/finance/ubs-tells-employees-more-milestones-cs-merger-be-announced-aug-31-2023-08-11/"
-// urlToImage: "https://www.reuters.com/resizer/TXIBCL2x67TpGxKogk3et6OEqQ4=/1200x628/smart/filters:quality(80)/cloudfront-us-east-2.images.arcpublishing.com/reuters/ARX2KBX66ZPXRMGHYY7YIYG64U.jpg"
 
 function fetchAllPages() {
   let page = 1;
