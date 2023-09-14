0d0df2927ad61edb507ae31c67ab6a353ed8bf53
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-09-14 21:14:17 +0200

    Deleted news api code

--- scripts/api.js
+++ scripts/api.js
@@ -1,83 +1,10 @@
-// require("dotenv").config();
-// const fetch = require("node-fetch");
+// https://danielmiessler.com/p/reddit-rss-functionality-explained/
 
-// Reuters:
-// Categories: Tech, business, science, world
-// ---
-// ---
-// ---
-// ---
-// ---
-// I'm mostly getting business from reuters, no science, new approach needed
-// Make sure I'm not getting any old data
-// Make sure I'm not getting any duplicates
-
-// const apiKey = process.env.NEWS_API_KEY;
-const apiUrl = "https://newsapi.org/v2/";
-const source = "reuters";
-const apiKey = "???";
-
-function fetchPage(page) {
-  return fetch(
-    `${apiUrl}everything?sources=${source}&page=${page}&apiKey=${apiKey}`
-  ).then((response) => {
-    if (!response.ok) console.error(`Http error: ${response.status}`);
-    return response.json();
-  });
-}
-
-function parse(dataArray) {
-  const parsed = dataArray
-    .flat()
-    .map((item) => {
-      const date = new Date(item.publishedAt);
-      if (item.url.includes("/science/")) item.category = "science";
-      else if (item.url.includes("/technology/")) item.category = "technology";
-      else if (item.url.includes("/business/")) item.category = "business";
-      else if (item.url.includes("/world/")) item.category = "world";
-      else item.category = "other";
-      return {
-        date: date.toLocaleString(),
-        title: item.title,
-        link: item.url,
-        description: item.description || "No description available",
-        source: item.source.name,
-        category: item.category,
-        content: item.content,
-        timestamp: new Date(item.publishedAt).getTime(),
-      };
-    })
-    .filter((item) => {
-      if (
-        item.category === "science" ||
-        item.category === "technology" ||
-        item.category === "business" ||
-        item.category === "world"
-      )
-        return item;
-    });
-
-  return parsed;
-}
-
-function fetchAllPages() {
-  let page = 1;
-  const results = [];
-
-  function fetchNextPage() {
-    return fetchPage(page).then((data) => {
-      // If no more items in array, stop calling and return results
-      if (data.articles.length === 0) return results;
-      results.push(data.articles);
-      page++;
-      // Recursively call itself until no more items in array
-      return fetchNextPage();
-    });
-  }
-  return fetchNextPage();
-}
-
-// fetchAllPages()
-//   .then((dataArray) => parse(dataArray))
-//   .then((parsed) => parsed.forEach((item) => console.log(item)))
-//   .catch((error) => console.error(error));
+// const api = "https://rss-to-json-serverless-api.vercel.app/api?feedURL=";
+// fetch(`${api}https://www.reddit.com/r/artificial/top/.rss?limit=100`)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     // const dates = data.items.map((item) => new Date(item.published));
+//     // console.log(dates);
+//     console.log(data);
+//   });
