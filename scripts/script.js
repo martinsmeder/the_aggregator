dcba899e704fca5f9262e8cc71c58dadef6f033c
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-09-15 21:33:01 +0200

    Removed api.js

--- scripts/script.js
+++ scripts/script.js
@@ -10,8 +10,16 @@ const {
 const { db } = require("./firebase-cjs");
 const fetch = require("node-fetch");
 
-// 8. Try out github actions
-// 9. Start building the real thing, starting with API's, and adding RSS where needed
+// https://danielmiessler.com/p/reddit-rss-functionality-explained/
+
+// const api = "https://rss-to-json-serverless-api.vercel.app/api?feedURL=";
+// fetch(`${api}https://www.reddit.com/r/artificial/top/.rss?limit=100`)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     // const dates = data.items.map((item) => new Date(item.published));
+//     // console.log(dates);
+//     console.log(data);
+//   });
 
 // https://pmarca.substack.com/feed
 
