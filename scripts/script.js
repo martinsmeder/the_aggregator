cd177f4925c7fa94bdbcca537e2bcecbc244db8a
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-09-16 21:17:18 +0200

    Added separate file (rss.js) for handling feeds

--- scripts/script.js
+++ scripts/script.js
@@ -10,19 +10,6 @@ const {
 const { db } = require("./firebase-cjs");
 const fetch = require("node-fetch");
 
-// https://danielmiessler.com/p/reddit-rss-functionality-explained/
-
-// const api = "https://rss-to-json-serverless-api.vercel.app/api?feedURL=";
-// fetch(`${api}https://www.reddit.com/r/artificial/top/.rss?limit=100`)
-//   .then((response) => response.json())
-//   .then((data) => {
-//     // const dates = data.items.map((item) => new Date(item.published));
-//     // console.log(dates);
-//     console.log(data);
-//   });
-
-// https://pmarca.substack.com/feed
-
 const rss = (() => {
   const urls = [
     { category: "ai", url: "https://www.deepmind.com/blog/rss.xml" },
