e7c116c332797bfce0050d1a897b99ab737b74a6
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-09-20 20:21:51 +0200

    Removed tests temporarily

--- scripts/rss.js
+++ scripts/rss.js
@@ -7,71 +7,71 @@ const rssFeeds = (() => {
       url: "https://news.mit.edu/topic/mitmachine-learning-rss.xml",
       isReddit: false,
     },
-    {
-      category: "ai",
-      url: "http://www.sciencedaily.com/rss/computers_math/artificial_intelligence.xml",
-      isReddit: false,
-    },
-    {
-      category: "ai",
-      url: "https://www.reddit.com/r/artificial/top/.rss?limit=500",
-      isReddit: true,
-    },
-    {
-      category: "programming",
-      url: "https://www.reddit.com/r/programming/top/.rss?limit=500",
-      isReddit: true,
-    },
-    {
-      category: "programming",
-      url: "http://blog.stackoverflow.com/feed/",
-      isReddit: false,
-    },
-    {
-      category: "programming",
-      url: "http://githubengineering.com/atom.xml",
-      isReddit: false,
-    },
-    {
-      category: "science",
-      url: "https://www.reddit.com/r/science/top/.rss?limit=500",
-      isReddit: true,
-    },
-    {
-      category: "science",
-      url: "http://newsrss.bbc.co.uk/rss/newsonline_world_edition/science/nature/rss.xml",
-      isReddit: false,
-    },
-    {
-      category: "science",
-      url: "http://www.sciencedaily.com/rss/mind_brain/neuroscience.xml",
-      isReddit: false,
-    },
-    {
-      category: "business",
-      url: "https://www.reddit.com/r/business/top/.rss?limit=500",
-      isReddit: true,
-    },
-    {
-      category: "business",
-      url: "http://newsrss.bbc.co.uk/rss/newsonline_world_edition/business/rss.xml",
-      isReddit: false,
-    },
-    {
-      category: "business",
-      url: "http://www.forbes.com/entrepreneurs/index.xml",
-      isReddit: false,
-    },
-    {
-      category: "world",
-      url: "https://www.reddit.com/r/worldnews/top/.rss?limit=500",
-      isReddit: true,
-    },
-    {
-      category: "world",
-      url: "http://feeds.bbci.co.uk/news/world/rss.xml",
-      isReddit: false,
-    },
+    // {
+    //   category: "ai",
+    //   url: "http://www.sciencedaily.com/rss/computers_math/artificial_intelligence.xml",
+    //   isReddit: false,
+    // },
+    // {
+    //   category: "ai",
+    //   url: "https://www.reddit.com/r/artificial/top/.rss?limit=500",
+    //   isReddit: true,
+    // },
+    // {
+    //   category: "programming",
+    //   url: "https://www.reddit.com/r/programming/top/.rss?limit=500",
+    //   isReddit: true,
+    // },
+    // {
+    //   category: "programming",
+    //   url: "http://blog.stackoverflow.com/feed/",
+    //   isReddit: false,
+    // },
+    // {
+    //   category: "programming",
+    //   url: "http://githubengineering.com/atom.xml",
+    //   isReddit: false,
+    // },
+    // {
+    //   category: "science",
+    //   url: "https://www.reddit.com/r/science/top/.rss?limit=500",
+    //   isReddit: true,
+    // },
+    // {
+    //   category: "science",
+    //   url: "http://newsrss.bbc.co.uk/rss/newsonline_world_edition/science/nature/rss.xml",
+    //   isReddit: false,
+    // },
+    // {
+    //   category: "science",
+    //   url: "http://www.sciencedaily.com/rss/mind_brain/neuroscience.xml",
+    //   isReddit: false,
+    // },
+    // {
+    //   category: "business",
+    //   url: "https://www.reddit.com/r/business/top/.rss?limit=500",
+    //   isReddit: true,
+    // },
+    // {
+    //   category: "business",
+    //   url: "http://newsrss.bbc.co.uk/rss/newsonline_world_edition/business/rss.xml",
+    //   isReddit: false,
+    // },
+    // {
+    //   category: "business",
+    //   url: "http://www.forbes.com/entrepreneurs/index.xml",
+    //   isReddit: false,
+    // },
+    // {
+    //   category: "world",
+    //   url: "https://www.reddit.com/r/worldnews/top/.rss?limit=500",
+    //   isReddit: true,
+    // },
+    // {
+    //   category: "world",
+    //   url: "http://feeds.bbci.co.uk/news/world/rss.xml",
+    //   isReddit: false,
+    // },
   ];
 
   function getPromises() {
@@ -93,7 +93,7 @@ const rssFeeds = (() => {
   function parse(array, category, isReddit) {
     const oneYearAgo = getOneYearAgo();
     const parsedData = array.items
-      .filter((item) => new Date(item.published) > oneYearAgo)
+      // .filter((item) => new Date(item.published) > oneYearAgo)
       .map((item) => {
         const date = new Date(item.published);
         return {
