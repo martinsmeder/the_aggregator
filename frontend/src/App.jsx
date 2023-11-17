b58f1900d47c52e444c18dca599ba68d56c86040
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-11-17 21:02:58 +0100

    Added some dummy code in frontend to test firebase

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,32 @@
 /* eslint-disable react/jsx-no-target-blank */
 import firebase from "./logic";
 import { testDb } from "./firebase-test";
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [items, setItems] = useState([]);
 
-  firebase.getCategoryQuery(testDb, "ai", null).then((querySnapshot) => {
-    console.log("First: ");
-    querySnapshot.forEach((doc) => console.log(doc.data().title));
-  });
+  useEffect(() => {
+    firebase
+      .getCategoryQuery(testDb, "ai", null)
+      .then((querySnapshot) =>
+        querySnapshot.docs.map((doc) => doc.data().title)
+      )
+      .then((mapped) => setItems(mapped))
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setItems([]);
+      });
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Things</h1>
+      {Array.isArray(items) && items.length > 0 ? (
+        items.map((item, index) => <p key={index}>{item}</p>)
+      ) : (
+        <p>No items available</p>
+      )}
     </>
   );
 }
