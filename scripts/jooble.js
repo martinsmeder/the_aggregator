706ef17cf36d8f5c8bf18387a3497a26a158e9ad
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-11-22 20:54:03 +0100

    Added logic to get job data for multiple fields

--- scripts/jooble.js
+++ scripts/jooble.js
@@ -3,30 +3,58 @@ const fetch = require("node-fetch");
 
 const url = "https://jooble.org/api/";
 const key = process.env.JOOBLE_API_KEY;
-const params = {
-  keywords: "sales",
-  location: "USA",
-};
 
-const fetchData = async () => {
-  try {
-    const response = await fetch(url + key, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(params),
-    });
+const keywordsList = [
+  "full stack",
+  "web developer",
+  "software engineer",
+  "data analyst",
+  "UX/UI designer",
+];
+const location = "USA";
 
+const fetchData = (keywords) => {
+  const params = {
+    keywords,
+    location,
+  };
+
+  return fetch(url + key, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(params),
+  }).then((response) => {
     if (response.ok) {
-      const data = await response.json();
-      console.log(data);
-    } else {
-      console.error("Error:", response.statusText);
+      return response.json();
     }
-  } catch (error) {
-    console.error("Error:", error);
-  }
+    throw new Error("Error:", response.statusText);
+  });
 };
 
-fetchData();
+const fetchAllData = () => {
+  const promises = keywordsList.map((keyword) => fetchData(keyword));
+
+  Promise.allSettled(promises)
+    .then((results) => {
+      results.forEach((result, index) => {
+        if (result.status === "fulfilled") {
+          console.log(
+            `Data for keyword "${keywordsList[index]}":`,
+            result.value
+          );
+        } else {
+          console.error(
+            `Error for keyword "${keywordsList[index]}":`,
+            result.reason.message
+          );
+        }
+      });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
+fetchAllData();
