05117f8b3c10fb4e121c0d2be859c28a8e390b20
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-12-02 18:23:55 +0100

    Added checkFeeds to gather dead URLs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,36 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-no-target-blank */
-import { getCategoryQuery, getHomeQuery } from "./database-logic";
+import { getSingleQuery } from "./database-logic";
 import { testDb } from "./firebase-test";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [snapshot, setSnapshot] = useState([]);
+  // const [snapshot, setSnapshot] = useState([]);
   const [items, setItems] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    getCategoryQuery(testDb, "ai", null)
+    getSingleQuery(testDb, "summaries")
       .then((querySnapshot) => {
-        setSnapshot(querySnapshot);
-        return querySnapshot.docs.map((doc) => doc.data().title);
+        // setSnapshot(querySnapshot);
+        return querySnapshot.docs.map((doc) => doc.data().summary);
       })
       .then((mapped) => setItems(mapped))
       .catch((error) => setError(error))
       .finally(() => setLoading(false));
   }, []);
 
-  function handleLoadClick(snapshot) {
-    getCategoryQuery(testDb, "ai", snapshot)
-      .then((querySnapshot) => {
-        setSnapshot(querySnapshot);
-        return querySnapshot.docs.map((doc) => doc.data().title);
-      })
-      .then((mapped) => {
-        setItems((itemsCopy) => [...itemsCopy, ...mapped]);
-      });
-  }
+  // function handleLoadClick(snapshot) {
+  //   getCategoryQuery(testDb, "ai", snapshot)
+  //     .then((querySnapshot) => {
+  //       setSnapshot(querySnapshot);
+  //       return querySnapshot.docs.map((doc) => doc.data().title);
+  //     })
+  //     .then((mapped) => {
+  //       setItems((itemsCopy) => [...itemsCopy, ...mapped]);
+  //     });
+  // }
 
   if (error) return <p>Error: {error}</p>;
   if (loading) return <p>Loading...</p>;
@@ -43,7 +43,7 @@ function App() {
       ) : (
         <p>No items available</p>
       )}
-      <button onClick={() => handleLoadClick(snapshot)}>Load more</button>
+      {/* <button onClick={() => handleLoadClick(snapshot)}>Load more</button> */}
     </>
   );
 }
