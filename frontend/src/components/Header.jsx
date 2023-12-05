a2c1796068c9389f253b1628e46bd467a9ab7c67
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-12-05 21:01:54 +0100

    Added icons to header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,11 +6,20 @@ export default function Header() {
       <h1>THE AGGREGATOR</h1>
 
       <nav>
-        <Link to="/">Summaries</Link>
+        <Link to="/">
+          <img src="summaries.png" alt="Summaries" />
+          <p>Summaries</p>
+        </Link>
 
-        <Link to="/feeds">Feeds</Link>
+        <Link to="/feeds">
+          <img src="feeds.png" alt="Feeds" />
+          <p>Feeds</p>
+        </Link>
 
-        <Link to="/trends">Trends</Link>
+        <Link to="/trends">
+          <img src="trends.png" alt="Trends" />
+          <p>Trends</p>
+        </Link>
       </nav>
     </header>
   );
