655c01304f78b49357bf21aadf4c829ae4f5e5f9
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-12-07 18:54:25 +0100

    Added some more style to footer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "./styles/root.css";
 import "./styles/header.css";
+import "./styles/footer.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
