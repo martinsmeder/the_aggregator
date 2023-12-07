df760f2d9228c4f9559aae4b152724494c68357a
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-12-07 18:54:25 +0100

    Added some more style to footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,10 +5,27 @@ export default function Footer() {
         target="_blank"
         href="https://github.com/martinsmeder"
         rel="noreferrer"
+        className="github"
       >
         <p>Code by Martin Smeder</p>
         <img src="github.png" alt="Github" />
       </a>
+
+      <div className="apis">
+        <a
+          target="_blank"
+          href="https://huggingface.co/facebook/bart-large-cnn"
+          rel="noreferrer"
+        >
+          <p>AI model hosted by HF</p>
+          <img src="hf.png" alt="Huggingface" />
+        </a>
+
+        <a target="_blank" href="https://jooble.org/api/about" rel="noreferrer">
+          <p>Job data aggregated by</p>
+          <img src="jooble.png" alt="Jooble" />
+        </a>
+      </div>
     </footer>
   );
 }
