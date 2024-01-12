33cd116eb1d3abfeeeeb676cebd0fa10436b9011
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2024-01-12 18:10:09 +0100

    New api for feeds since old one is removed

--- scripts/rss.js
+++ scripts/rss.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-shadow */
+require("dotenv").config();
 const fetch = require("node-fetch");
 const miscHelpers = require("./utils");
 
@@ -37,10 +38,13 @@ const rssFeeds = (() => {
   ];
 
   function getPromises(urls) {
-    const api = "https://rss-to-json-serverless-api.vercel.app/api?feedURL=";
+    const apiUrl = "https://api.rss2json.com/v1/api.json";
+
     const promises = urls.map((source) =>
       // Create an array of promises by fetching each source in urls array
-      fetch(api + source.url).then((response) => {
+      fetch(
+        `${apiUrl}?rss_url=${source.url}&api_key=${process.env.RSS_API_KEY}&count=1000`
+      ).then((response) => {
         if (response.ok) return response;
         return null;
       })
@@ -103,6 +107,28 @@ const rssFeeds = (() => {
   };
 })();
 
-rssFeeds.checkFeeds(rssFeeds.urls).then((result) => console.log(result));
+// rssFeeds.checkFeeds(rssFeeds.urls).then((result) => console.log(result));
+// rssFeeds
+//   .getRssData(rssFeeds.urls)
+//   .then((result) => console.log(result))
+//   .catch((error) => console.error(error));
+
+//  'https://news.mit.edu/topic/mitmachine-learning-rss.xml',
+//  'http://www.sciencedaily.com/rss/computers_math/artificial_intelligence.xml',
+//  'http://feeds.arstechnica.com/arstechnica/science'
 
 module.exports = rssFeeds;
+
+// const apiUrl = "https://api.rss2json.com/v1/api.json";
+// const feedUrl = "https://news.mit.edu/topic/mitcomputers-rss.xml";
+// const apiKey = process.env.RSS_API_KEY;
+// const fullUrl = `${apiUrl}?rss_url=${feedUrl}&api_key=${apiKey}&count=1000`;
+
+// fetch(fullUrl)
+//   .then((response) => response.json())
+//   .then((result) => console.log(result.items[0].enclosure.link))
+//   .catch((error) => console.error(error));
+
+// // 'title', 'pubDate', 'link', 'guid', 'author', 'thumbnail',
+// // 'description', 'content', 'enclosure', 'categories'
+// // source: array.feed.title
