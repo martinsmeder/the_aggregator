5644c7b24491585cba4faba731ead45668b5614d
martinsmeder <119725728+martinsmeder@users.noreply.github.com>
2024-08-07 18:24:59 +0200

    Added parseNewsData()

--- scripts/news-script.js
+++ scripts/news-script.js
@@ -1,12 +1,28 @@
 require("dotenv").config();
 const fetch = require("node-fetch");
-const firestore = require("./database-logic");
 
-const key = process.env.NEWS_API_KEY;
+const apiUrl = "https://newsapi.org/v2/top-headlines?"
+const parameters = "country=us&category=technology"
+const apiKey = process.env.NEWS_API_KEY;
 
-fetch(`https://newsapi.org/v2/top-headlines?category=technology&apiKey=${key}`)
+function parseNewsData(array) {
+    return array.map(article => {
+        const date = new Date(article.publishedAt);
+        return {
+            source: article.author,
+            title: article.title,
+            url: article.url,
+            published: date.toLocaleString(),
+            timestamp: date.getTime() // Used to simplify sorting.
+        };
+    });
+}
+
+fetch(`${apiUrl}${parameters}&apiKey=${apiKey}`)
     .then(response => response.json())
-    .then(result => console.log(result))
+    .then(result => {
+        const articles = parseNewsData(result.articles);
+        console.log(articles);
+    })
+    .catch(error => console.error('Error:', error));
 
-// Get specific category from specific source possible?
-// Otherwise above soultion with database.  
